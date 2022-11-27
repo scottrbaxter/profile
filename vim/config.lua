797bfe033680e827c097422ace3249cf2236c23e
Scott Baxter <scottrbaxter@gmail.com>
2022-11-27 13:57:03 -0600

    fix: lunarvim changes for deprecation notices

--- vim/config.lua
+++ vim/config.lua
@@ -151,9 +151,8 @@ lvim.builtin.which_key.mappings["t"] = {
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
+-- lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 -- lvim.builtin.nvimtree.show_icons.git = 0
